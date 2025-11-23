7ada48d054ae30aea3fbcdc2743e19cb26919141
digitalnzcheil <digitalnz@cheil.com>
2025-11-24 02:31:22 +1300

    same changes as production database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,14 +2,16 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false)
-        ? { ca: env('DATABASE_CA') }
+        ? {
+            ca: env('DATABASE_CA'),
+            rejectUnauthorized: true,
+          }
         : false,
     },
     pool: {
